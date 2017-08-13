4c4e0d0793f5b2f6556e1235f985185c032f4772
Ian McGonigle <ian@ianmcgonigle.com>
2017-08-12 22:38:01 -0600

    Add a students page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { page: 'Home' });
 });
 
+router.get('/students', function(req, res, next) {
+  res.render('students', { page: 'Students' });
+});
+
 module.exports = router;
